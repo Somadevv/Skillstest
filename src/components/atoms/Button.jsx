e8a3f904a43d88ba67d5c201133f049cb86a2e02
Josh <josh@insitesdigital.com>
2023-03-10 19:42:16 +0000

    sass test

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './button.module.scss';
+const Button = () => {
+  return <div className={styles.component}>test</div>;
+};
+
+export default Button;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Button from '../components/atoms/Button';
 
 export default function Home() {
   return (
@@ -8,7 +9,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        <Button />
+      </main>
     </>
   );
 }
